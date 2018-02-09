6a7ee3252c7f26ecfeac8c8069ea61bd39c6cf47
Trey Simmons <yert511@gmail.com>
2018-02-09 13:07:40 -0600

    Remove SSL check from script

--- check_pure_ftpd.py
+++ check_pure_ftpd.py
@@ -13,7 +13,7 @@ import ftplib
 
 def main(argv):
   try:
-    opts, args = getopt.getopt(argv, "hi:u:s:", ["help=", "ip=", "user=", "with-ssl="])
+    opts, args = getopt.getopt(argv, "hi:u:", ["help=", "ip=", "user="])
     validOpts = parse_args(opts)
     checkFTPConnection(validOpts)
   except getopt.GetoptError as e:
@@ -21,8 +21,6 @@ def main(argv):
 
 # Parse, validate, and group command line arguments
 def parse_args(args):
-  withSSL = False
-
   for opt, arg in args:
     if opt in ("-h", "--help"):
       # Display help message and immediately quit program
@@ -32,28 +30,24 @@ def parse_args(args):
       ipAddress = arg
     elif opt in ("-u", "--user"):
       user = arg
-    elif opt in ("-s", "with-ssl"):
-      if arg in ("true", "True"):
-        withSSL = bool(arg)
 
   # Build standardized arrangement of FTP connection data
   # Verify that all required data is present
   try:
-    parsedOptions = (ipAddress, user, withSSL)
+    parsedOptions = (ipAddress, user)
     return parsedOptions
   except UnboundLocalError as e:
     return_response(2, "A required flag was not set. Make sure that an IP address and user have been set for this command.")
 
 # Construct help message trigged with "-h" or "--help" flag
 def helpMessage():
-  usage = "Usage: check_pure_ftpd.py [-h] -i I -u U [-s S]" + "\n"
+  usage = "Usage: check_pure_ftpd.py [-h] -i I -u U" + "\n"
   desc = "Check that an FTP server is running and accessible." + "\n"
   argHeader = "Arguments:"
   helpInfo = "-h, --help      Show info about this command, optional"
   ipInfo   = "-i, --ip        IP address of FTP server, string, required"
   userInfo = "-u, --user      User name for FTP server, string, required"
-  sslInfo  = "-s, --with-SSL  Connect with SSL, optional, boolean, defaults to false"
-  display = (usage, desc, argHeader, helpInfo, ipInfo, userInfo, sslInfo)
+  display = (usage, desc, argHeader, helpInfo, ipInfo, userInfo)
   helpText = "\n".join(str(s) for s in display)
   print helpText
 
@@ -65,29 +59,17 @@ def checkFTPConnection(options):
   errorOtherMsg = "The FTP server could not be reached or manipulated: "
   successMsg = "The FTP server is reachable, and is responsive."
 
-  if options[2] == True:
-    try:
-      ftps = ftplib.FTP_TLS(options[0], options[1], password)
-      ftps.prot_p()
-      ftps.cwd('upload')
-      ftps.quit()
-    except ftplib.error_reply as e:
-      return_response(3, errorReplyMsg + str(e).capitalize())
-    except (ftplib.error_temp, ftplib.error_perm, ftplib.error_proto) as e:
-      return_response(2, errorOtherMsg + str(e).capitalize())
-    else:
-      return_response(0, successMsg)
+  try:
+    ftps = ftplib.FTP_TLS(options[0], options[1], password)
+    ftps.prot_p()
+    ftps.cwd('upload')
+    ftps.quit()
+  except ftplib.error_reply as e:
+    return_response(3,  + str(e).capitalize())
+  except (ftplib.error_temp, ftplib.error_perm, ftplib.error_proto) as e:
+    return_response(2, errorOtherMsg + str(e).capitalize())
   else:
-    try:
-      ftp = ftplib.FTP(options[0], options[1], password)
-      ftp.cwd('upload')
-      ftp.quit()
-    except ftplib.error_reply as e:
-      return_response(3, errorReplyMsg + str(e).capitalize())
-    except (ftplib.error_temp, ftplib.error_perm, ftplib.error_proto) as e:
-      return_response(2, errorOtherMsg + str(e).capitalize())
-    else:
-      return_response(0, successMsg)
+    return_response(0, successMsg)
 
 # Load saved password for FTP user from "ftp_password" file
 def getPasswordFromFile():
